a4b4f35bc1385ae38a6bf4bf075c220262f30bc2
charliescript <gozione3@gmail.com>
2020-09-09 16:24:21 +0100

    made some changes

--- src/redux/cart/cartActions.js
+++ src/redux/cart/cartActions.js
@@ -1,7 +1,8 @@
 import { BUY_ITEM } from './cartTypes';
 
 export const addToCart = (product) => (dispatch, getState) => {
-  const cartItems = getState().cart
+  const cartItems = getState().cart.slice()
+  let alreadyExists = false;
   cartItems.forEach((x) => {
     if (x._id === product._id) {
       alreadyExists = true;
