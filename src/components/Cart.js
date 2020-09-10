c133702e66454cca258e141176e77385d0cab5af
charieScript <gozione3@gmail.com>
2020-09-10 16:54:50 +0100

    refactored the cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,16 +1,37 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect }  from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeFromCart } from '../redux/cart/cartActions';
 
 function Cart() {
-  const cart = useSelector(state => state.cart)
-  const newCart = Array.from(new Set(cart.map(JSON.stringify))).map(JSON.parse)
-
-  console.log(newCart)
+  const dispatch = useDispatch()
+  const [total, setTotal ] = useState(0)
+  const cart = useSelector((state) => state.cart);
+  function deleteItem() {
+    dispatch(removeFromCart(cart.id))
+  }
+  useEffect(() => {
+    const total = cart.length !== 0 ? cart.reduce((prev, curr) => prev.total + curr.total): null;
+    setTotal(total)
+  }, [cart, total])
   return (
-    <div>
-      
+    <div className="px-32 my-6">
+      {cart.length !== 0 ? (
+        cart.map((item, index) => (
+          <div key={index}>
+            <img src={item.image} alt="" className="w-64" />
+            <p>
+              {item.count} X {item.price}
+            </p>
+            <button className="bg-red-700 m-1 py-1 px-2 text-sm rounded-sm hover:bg-red-900 outline-none shadow-sm" onClick={deleteItem}>
+              Remove from Cart
+            </button>
+            
+          </div>
+        ))
+      ): 'No item added'}
+      {/* <p>Total: {total}</p> */}
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
