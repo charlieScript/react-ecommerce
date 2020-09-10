a9c17f8112018fa613f10cf2f749fa48fb896b49
charieScript <gozione3@gmail.com>
2020-09-10 16:55:19 +0100

    added a little tweak to redux store

--- src/redux/cart/cartActions.js
+++ src/redux/cart/cartActions.js
@@ -1,19 +1,38 @@
-import { BUY_ITEM } from './cartTypes';
+import { ADD_TO_CART, REMOVE_FROM_CART } from './cartTypes';
 
 export const addToCart = (product) => (dispatch, getState) => {
-  const cartItems = getState().cart.slice()
+  const cart = getState().cart.slice();
   let alreadyExists = false;
-  cartItems.forEach((x) => {
-    if (x._id === product._id) {
+  cart.forEach((x) => {
+    if (x.id === product.id) {
       alreadyExists = true;
       x.count++;
+      x.total = x.count * x.price;
     }
   });
   if (!alreadyExists) {
-    cartItems.push({ ...product, count: 1 });
+    cart.push({ ...product, count: 1});
   }
   dispatch({
-    type: BUY_ITEM,
-    payload: { cartItems },
-  })
+    type: ADD_TO_CART,
+    payload: cart,
+  });
+  calculateCart(cart)
 };
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  let cart = getState().cart.slice();
+  cart = cart.filter(item => item.id === id)
+  dispatch({
+      type: REMOVE_FROM_CART,
+      payload: cart
+    })
+};
+
+function calculateCart(cart) {
+  const total =
+    cart.length !== 0
+      ? cart.reduce((prev, curr) => prev.total + curr.total)
+      : null;
+  console.log(total)
+}
--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,17 +1,93 @@
-import { BUY_ITEM } from './cartTypes';
+import { ADD_TO_CART, REMOVE_FROM_CART } from './cartTypes';
 import { addItem, incrementCart } from './cartUtils';
 
 const initialItems = {
+  totalPrice: 0,
   index: 0,
+  items: [
+    {
+      id: 0,
+      name: 'book',
+      image: 'https://dummyimage.com/150X100/d3d4e3/000000',
+      price: 5000,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 1,
+      name: 'iphone',
+      image: 'https://dummyimage.com/150X100/d3d4e3/000000',
+      price: 7000,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 2,
+      name: 'earphone',
+      image: 'https://dummyimage.com/150X100/d3d4e3/000000',
+      price: 500,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 3,
+      name: 'beans',
+      image: 'https://dummyimage.com/150X100/d3d4e3/000000',
+      price: 3000,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 4,
+      name: 'shoes',
+      image: 'https://dummyimage.com/150X100/d3d4e3/000000',
+      price: 1200,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 5,
+      name: 'egg',
+      image: 'https://dummyimage.com/150X100/d3d4e3/000000',
+      price: 500,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 6,
+      name: 'pizza',
+      image: 'https://dummyimage.com/150X100/d3d4e3/000000',
+      price: 30,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 7,
+      name: 'spag',
+      image: 'https://dummyimage.com/150X100/d3d4e3/000000',
+      price: 200,
+      count: 0,
+      total: 0
+    },
+  ],
   cart: [],
 };
 
 const cartReducer = (state = initialItems, action) => {
   switch (action.type) {
-    case BUY_ITEM:
+    case ADD_TO_CART:
       return {
         ...state,
-        cart: {cart:  action.payload.cartItems}
+        index: state.index + 1,
+        cart: action.payload,
+        totalPrice: action.total
+      }
+    case REMOVE_FROM_CART: 
+      return {
+        ...state,
+        index: state.index - 1,
+        cart: action.payload,
+        totalPrice: action.total
       }
     default:
       return state;
